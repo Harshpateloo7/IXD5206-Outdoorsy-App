36cec6b598efc02ce31fbbb7d0f130d2c0b53834
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-04-21 17:07:24 -0400

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+let check = document.querySelector(".hamburger");
+let nav = document.querySelector('.nav-menu');
+check.addEventListener('click', event => {
+    nav.classList.toggle('active');
+})
